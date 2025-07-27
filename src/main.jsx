c49b56c488f34e112937c8ef5ecd9a01b6bd8d0a
N-Abhishek-S <nagargojeabhishek96@gmail.com>
2025-07-27 20:20:29 +0530

    Main.jsx error solved

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,28 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
-import './index.css';  
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import './index.css';
 import Home from './components/Home.jsx';
 import Gallery from './components/Gallery.jsx';
 import Contact from './components/Contact.jsx';
 import About from './components/About.jsx';
 import Cars from './components/Cars.jsx';
-import '@fortawesome/fontawesome-free/css/all.min.css'
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route index element={<Home />} />
-      <Route path='gallery' element={<Gallery />} />
-      <Route path='about' element={<About />} />
-      <Route path='contact' element={<Contact />} />
-      <Route path='cars' element={<Cars />} />
-    </Route>
-  ),
-  {
-    basename: '/Tesla_CarshworoomWeb' // Add this line (no trailing slash)
-  }
-);
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="cars" element={<Cars />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   </StrictMode>
 );
+
