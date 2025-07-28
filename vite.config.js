0041c2247e6637a7e50164f69011daf45c6c1a4e
N-Abhishek-S <nagargojeabhishek96@gmail.com>
2025-07-28 19:53:56 +0530

    Vite-Config error solved

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Tesla_CarshworoomWeb/',
+  base: '/Tesla_CarShowroomWeb/',
   plugins: [react()],
   build: {
     outDir: 'dist',
